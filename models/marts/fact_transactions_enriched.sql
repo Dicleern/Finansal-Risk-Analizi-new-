e6d86175211874396fcd08ae658e6fcdd6c23a5e
Elif Çal <elifcal01@gmail.com>
2026-01-20 18:17:09 +0000

    add regional sector stats and distance analysis reporting

---
+++ models/marts/fact_transactions_enriched.sql
@@ -0,0 +1,35 @@
+/*  Model: fact_transactions_enriched
+    Purpose: Adding distance and digitalization metrics to analyze each operation.
+    Author: Elif
+*/
+
+WITH master_table AS ( SELECT * FROM {{ ref('int_transaction_leftjoin') }} )
+
+SELECT
+    
+      transaction_id
+    , user_id
+    , transaction_date
+    , use_chip
+
+    , CASE 
+         WHEN use_chip = 'Online Transaction' THEN 'Online'
+         ELSE 'Physical Transaction'
+      END AS channel_type
+    
+    /* I measured the distance between the user's house and the seller in meters, 
+    then divided by 1000 to convert it to kilometers.*/
+    , ST_DISTANCE(
+        ST_GEOGPOINT(user_home_lon, user_home_lat), 
+        ST_GEOGPOINT(merchant_lon, merchant_lat)) / 1000 AS distance_km
+    
+    , merchant_city
+    , merchant_state
+    , category_name 
+    , amount
+    , card_on_dark_web
+    , errors
+
+FROM master_table
+-- I only included those with filled coordinates to avoid errors.
+WHERE user_home_lat IS NOT NULL AND merchant_lat IS NOT NULL
---
+++ models/marts/rpt_regional_sector_stats.sql
@@ -0,0 +1,35 @@
+/*  Model: rpt_regional_sector_stats
+    Purpose: I designed this reporting model to aggregate transaction metrics by city and sector.
+    Author: Elif
+*/
+
+WITH enriched_data AS (
+    -- I am pulling data from my enhanced fact table where I previously added distance and channel logic.
+    SELECT * FROM {{ ref('fact_transactions_enriched') }})
+
+SELECT
+
+      merchant_state
+    , merchant_city
+    , category_name
+
+    -- I calculated the total count and volume to identify the most active sectors in each region.
+    , COUNT(transaction_id) as total_transactions
+    , SUM(amount) as total_volume
+    , AVG(amount) as avg_ticket_size
+
+    -- I counted online transactions specifically to measure digital adoption.
+    , COUNT(CASE WHEN channel_type = 'Online' THEN 1 END) as online_transaction_count
+    
+    -- I calculated the digitalization rate by dividing online transactions by the total.
+    -- This allows me to see which cities are shifting to e-commerce faster.
+    , ROUND(COUNT(CASE WHEN channel_type = 'Online' THEN 1 END) / COUNT(*) ,2) as digitalization_rate
+
+    -- I aggregated high-risk transactions to create a regional risk heatmap.
+    , COUNT(CASE WHEN card_on_dark_web = true THEN 1 END) as fraud_risk_count
+
+FROM enriched_data
+
+GROUP BY   merchant_state
+         , merchant_city
+         , category_name
