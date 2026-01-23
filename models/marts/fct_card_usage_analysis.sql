4217bb9e617982eae6afaaac1b31f57fcd35835a
Dicle Ürün Dodurga <dicleurun500@gmail.com>
2026-01-23 11:13:39 +0000

    Second update for the card usage analysis

--- models/marts/fct_card_usage_analysis.sql
+++ models/marts/fct_card_usage_analysis.sql
@@ -8,9 +8,9 @@ select
         partition by client_id 
         order by kullanim_sayisi desc
     ) as kullanim_sirasi,
-    date_diff(son_kullanma_tarihi, son_islem_tarihi, day) as son_islem_ve_expiry_farki_gun,
+    date_diff(son_kullanma_tarihi, '2020-02-29', day) as son_islem_ve_expiry_farki_gun,
     case 
-        when date_diff(son_kullanma_tarihi, son_islem_tarihi, day) <= 60 then 1 
+        when date_diff(son_kullanma_tarihi, '2020-02-29', day) <= 60 then 1 
         else 0 
     end as son_kullanma_kritik_mi
 from base_data
@@ -20,3 +20,6 @@ order by client_id, kullanim_sirasi
 -- müşteri idlerine göre kartların en çok kullanılma durumlarını sıraladım 
 -- son işlem tarihi ve expires arasındaki tarih farkını hesapladım
 -- çıkan tarih farkı 60 ve 60'dan az ise 0 çok ise 1 yazdırdım.
+-- update ettim
+-- son kullanım tarihi - son işlem tarihi yapmak yerine user data tanlosundan analizin çekildi tarihi hesapladım.
+-- 29.02.2020 yılına göre yaş hesaplaması yapılmış current ageler yazılmış o tarihe göre de son kullanıma ne kadar kaldığını hesapladım.
